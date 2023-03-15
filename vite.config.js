41bd68a9cc620ac7a3c1322a3d3eb821811152d8
Carlos <36555654+lospoy@users.noreply.github.com>
2023-03-14 23:27:02 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import vue from '@vitejs/plugin-vue';
-import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-const defineConfig({
+export default defineConfig({
   plugins: [vue()],
   server: {
     port: 8000
   }
 })
-
-export default defineConfig;
