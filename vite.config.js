8db3b682956f739c726c5750ddec2a3fe8716609
Carlos <36555654+lospoy@users.noreply.github.com>
2023-06-22 23:20:19 -0400

    add eslint

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({})],
+  plugins: [vue({}), eslintPlugin()],
   server: {
     port: 8000
   }
