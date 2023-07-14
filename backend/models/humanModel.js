4b36a691932e5ea6506fc12748c485c1b824ceb9
Carlos <36555654+lospoy@users.noreply.github.com>
2023-07-14 15:11:02 -0400

    trainingData schema update

--- backend/models/humanModel.js
+++ backend/models/humanModel.js
@@ -2,109 +2,135 @@
 const mongoose = require('mongoose')
 
 const humanSchema = mongoose.Schema(
-    {
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: false,
-            ref: 'User',
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: false,
+      ref: 'User'
+    },
+    name: {
+      type: Object,
+      required: [true, 'Name required'],
+      properties: {
+        first: {
+          type: String,
+          required: [true, 'name.firstName required']
         },
-        name: {
-            type: Object,
-            required: [true, 'Name required'],
-            properties: {
-                first: {
-                    type: String,
-                    required: [true, 'name.firstName required'],
-                },
-                last: {
-                    type: String,
-                    required: [true, 'name.lastName required'],
-                },
-                nickName: {
-                    type: String,
-                    required: false
-                },
-                pronouns: {
-                    type: String,
-                    required: false
-                },
-            }
+        last: {
+          type: String,
+          required: [true, 'name.lastName required']
         },
-        trainingStatus: {
-            type: Object,
-            required: [true, 'Current training status required.'],
-            properties: {
-                active: {
-                    type: Boolean,
-                    required: false,
-                    default: true
-                },
-                injured: {
-                    type: Boolean,
-                    required: false,
-                },
-                retired: {
-                    type: Boolean,
-                    required: false,
-                },
+        nickName: {
+          type: String,
+          required: false
+        },
+        pronouns: {
+          type: String,
+          required: false
+        }
+      }
+    },
+    trainingData: {
+      type: Object,
+      required: true,
+      properties: {
+        status: {
+          type: Object,
+          required: [true, 'Current training status required.'],
+          properties: {
+            active: {
+              type: Boolean,
+              required: false,
+              default: true
             },
-        },
-        trainingHistory: {
-            type: Object,
-            required: [false, 'Specify training history. At least "brand new".' ],
-            properties: {
-                belt: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    required: [false, 'trainingHistory.belt required.'],
-                    ref: 'Belt'
-                },
-                lastSession: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    required: [false, 'trainingHistory.lastSession required.'],
-                    ref: 'Session'
-                },
-                firstSession: {
-                    type: mongoose.Schema.Types.ObjectId,
-                    required: [false, 'trainingHistory.firstSession required.'],
-                    ref: 'Session'
-                },
-                totalTrained: {
-                    type: Object,
-                    required: [false, 'Specify total trained. Minimum 1 class.'],
-                    properties: {
-                        sessions: {
-                            type: Number,
-                            required: [false, 'totalTrained.sessions required.'],
-                        },
-                        time: {
-                            type: Object,
-                            required: [false, 'totalTrained.time required.'],
-                            properties: {
-                                years: {
-                                    type: Number,
-                                    required: false
-                                }
-                            }
-                        }
-                    }
-                }
+            injured: {
+              type: Boolean,
+              required: false
+            },
+            retired: {
+              type: Boolean,
+              required: false
             }
+          }
         },
-        crm: {
+        belt: {
+          current: {
+            color: {
+              type: Object,
+              required: false,
+              properties: {
+                white: {
+                  type: Boolean,
+                  required: false
+                },
+                blue: {
+                  type: Boolean,
+                  required: false
+                },
+                purple: {
+                  type: Boolean,
+                  required: false
+                },
+                brown: {
+                  type: Boolean,
+                  required: false
+                },
+                black: {
+                  type: Boolean,
+                  required: false
+                }
+              }
+            },
+            stripes: {
+              type: Object,
+              required: false,
+              properties: {
+                one: {
+                  type: Boolean,
+                  required: false
+                },
+                two: {
+                  type: Boolean,
+                  required: false
+                },
+                three: {
+                  type: Boolean,
+                  required: false
+                },
+                four: {
+                  type: Boolean,
+                  required: false
+                }
+              }
+            },
+            dateAwarded: {
+              type: Date,
+              required: true
+            }
+          },
+          history: {
             type: Object,
             required: false,
-            properties: {
-                id: {
-                    type: String,
-                    required: [true, 'crm.id required'],
-                    unique: true,
-                },
-            }
-        },
+            properties: {}
+          }
+        }
+      }
     },
-    {
-        timestamps: true,
-    },
+    crm: {
+      type: Object,
+      required: false,
+      properties: {
+        id: {
+          type: String,
+          required: [true, 'crm.id required'],
+          unique: true
+        }
+      }
+    }
+  },
+  {
+    timestamps: true
+  }
 )
 
 module.exports = mongoose.model('Human', humanSchema)
